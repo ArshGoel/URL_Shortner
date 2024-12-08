cabd43220cc3063d8e34574fbdbb95b202c916aa
Arsh Goel <arshgoel16@gmail.com>
2024-12-08 12:16:50 +0530

    Initial Commit

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -118,11 +118,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -131,13 +126,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Added Manually
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static")
-] 
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "legend163104@gmail.com"
-EMAIL_HOST_PASSWORD = "rwudmuzzrqaneemx"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build',"static")
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "legend163104@gmail.com"
+# EMAIL_HOST_PASSWORD = "rwudmuzzrqaneemx"
