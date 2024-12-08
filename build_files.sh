238fb97d48074841419057a89f2893fbd43a124b
Arsh Goel <arshgoel16@gmail.com>
2024-12-08 13:34:29 +0530

    Initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo "BUILD START"
 python -m pip install -r requirements.txt
+python manage.py makemigrations 
+python manage.py migrate
 python manage.py collectstatic --noinput --clear
 echo "BUILD END"
