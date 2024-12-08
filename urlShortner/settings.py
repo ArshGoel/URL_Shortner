c02f3fc13c199f4a7c48218dcdffab283a89d990
Arsh Goel <arshgoel16@gmail.com>
2024-12-08 12:26:19 +0530

    Initial commit

--- urlShortner/settings.py
+++ urlShortner/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0aqqxpq*hoi)spb$kmwku%3x!@g^hn-jeu55_k$8vs)k+6izd=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
